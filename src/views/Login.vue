b0860dc0449c5088d788f1a1036f11eb7c9e2393
Mr.King <825551308@qq.com>
2022-08-22 17:53:23 +0800

    第一次commit

--- src/views/Login.vue
+++ src/views/Login.vue
@@ -4,29 +4,41 @@
     <input id="account" placeholder="请输入账号"/>
     <input id="pwd" placeholder="请输入密码"/>
     <button @click="loginActionFunc">确 认</button>
-    <p>
-      <router-link to="homeView">进入首页</router-link>
-    </p>
-    <el-row>
-      <el-button type="primary">主要num按钮</el-button>
-    </el-row>
-    <SSJButton title="自定义按钮" @btnClick="customBtnAction"></SSJButton>
+<!--    <p>-->
+<!--      <router-link to="homeView">进入首页</router-link>-->
+<!--    </p>-->
+<!--    <el-row>-->
+<!--      <el-button type="primary">主要num按钮</el-button>-->
+<!--    </el-row>-->
+<!--    <SSJButton title="自定义按钮" @btnClick="customBtnAction"></SSJButton>-->
   </div>
 
 </template>
 
 <script>
 
-import router from "../router";
+// import router from "../router";
 import SSJButton from "@/components/SSJButton";
+import {getRegisterCode} from "@/api/api";
 
 export default {
   components: {SSJButton},
   methods: {
     loginActionFunc (){
+      console.log("点击了按钮")
       // this.$emit()
-      alert("登录成功！即将跳转首页")
-      router.push("/home")
+      // alert("登录成功！即将跳转首页")
+      // router.push("/home")
+
+      getRegisterCode({"username":"13396551781"}).then(
+          (res) => {
+            console.log("请求结束了\\n")
+            console.log(res)
+          }
+      ).catch((err) =>{
+        console.log("请求错误信息："+ err)
+      })
+
     },
     customBtnAction(val){
       alert("父组件-SSJButton点击附带参数："+val)
--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,19 @@
 const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
-  transpileDependencies: true
+  transpileDependencies: true,
+  //添加以下代码
+  devServer: {
+    proxy: {
+      '/api': {
+        target: 'http://192.168.50.18:9091', //接口域名
+        changeOrigin: true,             //是否跨域
+        ws: true,                       //是否代理 websockets
+        secure: true,                   //是否https接口
+        pathRewrite: {                  //路径重置
+          '^/api': ''
+        }
+      }
+    }
+  }
+
 })
