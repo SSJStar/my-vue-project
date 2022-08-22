894fd0282f901da04c49d142e74ceeb5464ea298
Mr.King <825551308@qq.com>
2022-08-22 11:20:06 +0800

    第一次commit

---
+++ src/views/Home.vue
@@ -0,0 +1,39 @@
+<!--<template>-->
+<!--  <div>-->
+<!--    <h1>这是首页</h1>-->
+<!--  </div>-->
+<!--</template>-->
+
+<!--<script></script>-->
+
+<!--<style>-->
+<!--</style>-->
+
+<template>
+  <div>
+    <h1>这是首页</h1>-->
+<!--    <v-form-render :form-json="formJson" :form-data="formData" :option-data="optionData" ref="vFormRef">-->
+<!--    </v-form-render>-->
+    <el-button type="primary" @click="submitForm">Submit</el-button>
+  </div>
+</template>
+
+<script setup>
+import { ref, reactive } from 'vue'
+import { ElMessage } from 'element-plus'
+
+const formJson = reactive({"widgetList":[{"key":56625,"type":"date","icon":"date-field","formItemFlag":true,"options":{"name":"date68160","label":"date","labelAlign":"label-left-align","type":"date","defaultValue":null,"placeholder":"","columnWidth":"200px","size":"","autoFullWidth":true,"labelWidth":null,"labelHidden":false,"readonly":false,"disabled":false,"hidden":false,"clearable":true,"editable":false,"format":"YYYY-MM-DD","valueFormat":"YYYY-MM-DD","required":false,"requiredHint":"","validation":"","validationHint":"","customClass":[],"labelIconClass":null,"labelIconPosition":"rear","labelTooltip":null,"onCreated":"","onMounted":"","onChange":"","onFocus":"","onBlur":"","onValidate":""},"id":"date68160"},{"key":104089,"type":"card","category":"container","icon":"card","widgetList":[],"options":{"name":"card42878","label":"card","hidden":false,"folded":false,"showFold":true,"cardWidth":"100%","shadow":"never","customClass":[]},"id":"card42878"},{"key":50519,"type":"grid","category":"container","icon":"grid","cols":[{"type":"grid-col","category":"container","icon":"grid-col","internal":true,"widgetList":[],"options":{"name":"gridCol90469","hidden":false,"span":12,"offset":0,"push":0,"pull":0,"responsive":false,"md":12,"sm":12,"xs":12,"customClass":""},"id":"grid-col-90469"},{"type":"grid-col","category":"container","icon":"grid-col","internal":true,"widgetList":[],"options":{"name":"gridCol18273","hidden":false,"span":12,"offset":0,"push":0,"pull":0,"responsive":false,"md":12,"sm":12,"xs":12,"customClass":""},"id":"grid-col-18273"},{"type":"grid-col","category":"container","icon":"grid-col","internal":true,"widgetList":[],"options":{"name":"gridCol6651","hidden":false,"span":12,"offset":0,"push":0,"pull":0,"responsive":false,"md":12,"sm":12,"xs":12,"customClass":""},"id":"grid-col-6651"}],"options":{"name":"grid29017","hidden":false,"gutter":12,"colHeight":null,"customClass":""},"id":"grid29017"}],"formConfig":{"modelName":"formData","refName":"vForm","rulesName":"rules","labelWidth":80,"labelPosition":"left","size":"","labelAlign":"label-left-align","cssCode":"","customClass":[],"functions":"","layoutType":"PC","jsonVersion":3,"onFormCreated":"","onFormMounted":"","onFormDataChange":""}})
+const formData = reactive({})
+const optionData = reactive({})
+const vFormRef = ref(null)
+
+const submitForm = () => {
+  vFormRef.value.getFormData().then(formData => {
+    // Form Validation OK
+    alert( JSON.stringify(formData) )
+  }).catch(error => {
+    // Form Validation failed
+    ElMessage.error(error)
+  })
+}
+</script>
---
+++ src/views/Login.vue
@@ -0,0 +1,69 @@
+<template>
+  <div id="login">
+    <h1 class="myH1">欢迎登录</h1>
+    <input id="account" placeholder="请输入账号"/>
+    <input id="pwd" placeholder="请输入密码"/>
+    <button @click="loginActionFunc">确 认</button>
+    <p>
+      <router-link to="homeView">进入首页</router-link>
+    </p>
+    <el-row>
+      <el-button type="primary">主要num按钮</el-button>
+    </el-row>
+    <SSJButton title="自定义按钮" @btnClick="customBtnAction"></SSJButton>
+  </div>
+
+</template>
+
+<script>
+
+import router from "../router";
+import SSJButton from "@/components/SSJButton";
+
+export default {
+  components: {SSJButton},
+  methods: {
+    loginActionFunc (){
+      // this.$emit()
+      alert("登录成功！即将跳转首页")
+      router.push("/home")
+    },
+    customBtnAction(val){
+      alert("父组件-SSJButton点击附带参数："+val)
+    }
+  }
+}
+
+</script>
+
+<style>
+/* 定义样式：id="login" */
+/*#login{*/
+/*  background-color: slategray;*/
+/*}*/
+/* 定义样式：class="myH1" */
+.myH1{
+  color: cadetblue;
+}
+</style>
+
+
+
+
+<!--<template>-->
+<!--  <div id="test">-->
+<!--    <SSJButton title="测试一下" @btnClick="doSome" />-->
+<!--  </div>-->
+
+<!--</template>-->
+<!--<script>-->
+<!--import SSJButton from "../components/SSJButton";-->
+<!--export default {-->
+<!--  components: {SSJButton},-->
+<!--  methods: {-->
+<!--    doSome(val){-->
+<!--      alert("父组件打印："+val)-->
+<!--    }-->
+<!--  }-->
+<!--}-->
+<!--</script>-->
