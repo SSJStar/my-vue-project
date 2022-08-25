169a816b5682849e0981bc8b919a37fdb6ed8401
Mr.King <825551308@qq.com>
2022-08-25 17:34:13 +0800

    20220825

--- src/App.vue
+++ src/App.vue
@@ -32,6 +32,12 @@ export default {
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
+  /*margin-top: 60px;*/
+  /*margin: 0px;*/
+  /*padding: 0px;*/
+}
+body{
+  margin: 0px;
+  padding: 0px;
 }
 </style>
--- src/views/Login.vue
+++ src/views/Login.vue
@@ -1,78 +1,66 @@
 <template>
-  <div id="login">
-    <h1 class="myH1">欢迎登录</h1>
-<!--    id="account"  id="pwd"  -->
-<!--    <input ref="uName" placeholder="请输入账号"/>-->
-<!--    <input ref="pwd" placeholder="请输入密码"/>-->
-<!--    <button @click="loginActionFunc">确 认</button>-->
-<!--    <p>-->
-<!--      <router-link to="home">进入首页</router-link>-->
-<!--    </p>-->
-<!--    <el-row>-->
-<!--      <el-button type="primary">主要num按钮</el-button>-->
-<!--    </el-row>-->
-<!--    <SSJButton title="自定义按钮" @btnClick="customBtnAction"></SSJButton>-->
+  <div id="login" style="width: 100%;height: 100%;background-color: gray;position: relative">
+<!--    position: fixed-->
+<!--    text-align: right;justify-content: flex-end-->
+    <div style="background-color: bisque;width: 100%;height: 100%;position: absolute;">
+      <img src="@/assets/kejiBG.png" style="width: 100%;height: 100%; "/>
+    </div>
+<!--    <img src="@/assets/logo.png" style="width: 20%;height: 20%;position: absolute">-->
+    <div style="background-color:#323bb6;width: 20%;height: 20%; position: absolute;z-index: 1">
 
-<!--使用VForm3表单库-->
-    <v-form-render :form-json="formJson" :form-data="formData" :option-data="optionData" ref="vFormRef">
-    </v-form-render>
-    <el-button type="primary" @click="submitForm">Submit</el-button>
-
+      <h1 class="myH1">欢迎登录</h1>
+      <input ref="uName" placeholder="请输入账号"/><br/>
+      <input ref="pwd" placeholder="请输入密码"/><br/>
+      <button @click="loginActionFunc">确 认</button>
+    </div>
   </div>
 
+
+
+
+<!--  <div id="outer-div" style="height:80px;width:100px;border: 1px solid red;position: relative;">-->
+<!--    <div id="first-div" style="width:50%;height:100%;border-right: 2px solid #619e38;"></div>-->
+<!--    <div id="cell-icon" style="border:1px solid blue; position: absolute; top: 50%; left: 50%; transform:translate(-50%, -50%); background-color: #fff;z-index:1; ">anFAicon</div>-->
+<!--  </div>-->
+
+
+
+
+<!--  <div class="title clear fwb" style="overflow:hidden;position:relative;width: 100%;height: 100%;background-color: #42b983">-->
+<!--    <div class="author fl" style="color: red;overflow: hidden;position: absolute;top: 0px;right: 0px;">-->
+<!--      当前在线人数：<label id="onlineusernum"></label>人-->
+<!--    </div>-->
+<!--  </div>-->
 </template>
 
 <script setup>
 
-// import router from "../router";
+import router from "../router";
 // import SSJButton from "@/components/SSJButton";
-// import {getRegisterCode} from "@/api/api";
-//
+import {getRegisterCode, loginWithUNameAndPwd} from "@/api/api";
+import {ref} from 'vue'
 // export default {
-//   components: {SSJButton},
-//   methods: {
-//     loginActionFunc (){
-//       console.log("点击了按钮1:"+this.$refs.uName.value)
-//       // this.$emit()
-//       // alert("登录成功！即将跳转首页")
-//       // router.push("/home")
-//
-//       getRegisterCode({"username":this.$refs.uName.textContent}).then(
-//           (res) => {
-//             console.log("请求结束了\\n")
-//             console.log(res)
-//           }
-//       ).catch((err) =>{
-//         console.log("请求错误信息："+ err)
-//       })
-//
-//     },
-//     customBtnAction(val){
-//       alert("父组件-SSJButton点击附带参数："+val)
-//     }
-//   }
-// }
+  // components: {SSJButton},
+  // methods: {
+    function loginActionFunc(){
+      // console.log("点击了按钮1:"+this.$refs.uName.value)
+  const  e = ref(uName)
+  console.log("点击了按钮1:"+e.value)
+      // this.$emit()
+      // alert("登录成功！即将跳转首页")
+      // router.push("/home")
 
-import { ref, reactive } from 'vue'
-import { ElMessage } from 'element-plus'
-import {loginWithUNameAndPwd} from "@/api/api";
+      // getRegisterCode({"username":this.$refs.uName.textContent}).then(
+      //     (res) => {
+      //       console.log("请求结束了\\n")
+      //       console.log(res)
+      //     }
+      // ).catch((err) =>{
+      //   console.log("请求错误信息："+ err)
+      // })
 
-const formJson = reactive({"widgetList":[{"key":104100,ref:"accountInput","type":"input","icon":"text-field","formItemFlag":true,"options":{"name":"input95445","label":"账号","labelAlign":"","type":"text","defaultValue":"","placeholder":"","columnWidth":"200px","size":"","labelWidth":null,"labelHidden":false,"readonly":false,"disabled":false,"hidden":false,"clearable":true,"showPassword":false,"required":true,"requiredHint":"","validation":"","validationHint":"","customClass":[],"labelIconClass":null,"labelIconPosition":"rear","labelTooltip":null,"minLength":null,"maxLength":null,"showWordLimit":false,"prefixIcon":"","suffixIcon":"","appendButton":false,"appendButtonDisabled":false,"buttonIcon":"custom-search","onCreated":"","onMounted":"","onInput":"","onChange":"","onFocus":"","onBlur":"","onValidate":""},"id":"input95445"},{"key":104100,"type":"input","icon":"text-field","formItemFlag":true,"options":{"name":"input71664","label":"密码","labelAlign":"","type":"password","defaultValue":"","placeholder":"","columnWidth":"200px","size":"","labelWidth":null,"labelHidden":false,"readonly":false,"disabled":false,"hidden":false,"clearable":true,"showPassword":false,"required":true,"requiredHint":"","validation":"","validationHint":"","customClass":"","labelIconClass":null,"labelIconPosition":"rear","labelTooltip":null,"minLength":null,"maxLength":null,"showWordLimit":false,"prefixIcon":"","suffixIcon":"","appendButton":false,"appendButtonDisabled":false,"buttonIcon":"custom-search","onCreated":"","onMounted":"","onInput":"","onChange":"","onFocus":"","onBlur":"","onValidate":""},"id":"input71664"}],"formConfig":{"modelName":"formData","refName":"vForm","rulesName":"rules","labelWidth":80,"labelPosition":"left","size":"","labelAlign":"label-left-align","cssCode":"","customClass":[],"functions":"","layoutType":"PC","jsonVersion":3,"onFormCreated":"","onFormMounted":"","onFormDataChange":"","onFormValidate":""}})
-const formData = reactive({})
-const optionData = reactive({})
-const vFormRef = ref(null)
-
-const submitForm = () => {
-  vFormRef.value.getFormData().then(formData => {
-    // Form Validation OK
-    // alert( JSON.stringify(formData) )
-    let uName = formData.input95445
-    let pwd = formData.input71664
-    alert( "输入内容：用户名:" + uName + " 密码：" + pwd)
-
-
-    //开始请求
-    loginWithUNameAndPwd({"username":uName, "password": pwd}).then(
+      //开始请求
+      loginWithUNameAndPwd({"username":this.$refs.uName.textContent, "password": this.$refs.pwd.textContent}).then(
           (res) => {
             console.log("请求结束了\\n")
             console.log(res)
@@ -80,11 +68,11 @@ const submitForm = () => {
       ).catch((err) =>{
         console.log("请求错误信息："+ err)
       })
-  }).catch(error => {
-    // Form Validation failed
-    ElMessage.error(error)
-  })
-}
+
+    }
+  // }
+// }
+
 
 
 </script>
@@ -98,6 +86,34 @@ const submitForm = () => {
 .myH1{
   color: cadetblue;
 }
+
+
+
+
+
+
+/*#outer-div {*/
+/*  height:80px;*/
+/*  width:100px;*/
+/*  border: 1px solid red;*/
+/*  position: relative;*/
+/*}*/
+/*#first-div {*/
+/*  width:50%;*/
+/*  height:100%;*/
+/*  border-right: 2px solid #619e38*/
+/*}*/
+/*#cell-icon {*/
+/*  border:1px solid blue;*/
+/*  position: absolute;*/
+/*  top: 50%;*/
+/*  left: 50%;*/
+/*  transform:translate(-50%, -50%);*/
+/*  background-color: #fff;*/
+/*  z-index:1;*/
+/*}*/
+
+
 </style>
 
 
@@ -120,3 +136,16 @@ const submitForm = () => {
 <!--  }-->
 <!--}-->
 <!--</script>-->
+
+
+
+
+<!--&lt;!&ndash;    <p>&ndash;&gt;-->
+<!--&lt;!&ndash;      <router-link to="home">进入首页</router-link>&ndash;&gt;-->
+<!--&lt;!&ndash;    </p>&ndash;&gt;-->
+
+<!--&lt;!&ndash;    <el-row>&ndash;&gt;-->
+<!--&lt;!&ndash;      <el-button type="primary">主要num按钮</el-button>&ndash;&gt;-->
+<!--&lt;!&ndash;    </el-row>&ndash;&gt;-->
+
+<!--&lt;!&ndash;    <SSJButton title="自定义按钮" @btnClick="customBtnAction"></SSJButton>&ndash;&gt;-->
