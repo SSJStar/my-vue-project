e10672a364f7b08ae8d7f7fb9869cd8f3ed0090c
Mr.King <825551308@qq.com>
2022-08-31 11:30:56 +0800

    20220826 11:25

--- src/views/Login.vue
+++ src/views/Login.vue
@@ -1,42 +1,45 @@
 <template>
-  <div id="login" style="width: 100%;height: 100%;background-color: gray;">
+  <div id="login">
 
-    <div id="backgroundDIV" >
+    <div id="background-div" >
       <img src="@/assets/keji_bg.png"/>
     </div>
 
-    <div id="loginModuleDIV">
-<!--      现在使用的框架是:vue3 + element-plus-->
-      <h1 class="loginModuleDIV-title">欢迎登录</h1>
-      <h2 class="loginModuleDIV-subtitle">现在讲解的是 - 图形验证码</h2>
+    <div id="login-module-div">
+      <h1 class="lmd-title">欢迎登录</h1>
+      <h2 class="lmd-subtitle">现在使用的框架是:vue3 + element-plus</h2>
 
-<!--   账号   -->
-      <div class="div-el-input" style="margin-top: 40px;">
-        <el-input ref="uName" v-model="nameValue"  placeholder="请输入账号" :prefix-icon="Avatar"   @input="changeHandle($event)"></el-input>
+      <!-- 账号 -->
+      <div class="el-input-div" style="margin-top: 40px;">
+        <el-input v-model="loginInput.nameValue"  placeholder="请输入账号" :prefix-icon="Avatar"   @input="changeHandle($event)"></el-input>
       </div>
 
-<!--   密码   -->
-      <div class="div-el-input" style="margin-top: 20px;">
-        <el-input v-model="pwdValue" placeholder="请输入密码"  :prefix-icon="Lock" @input="pwdChangeHandle($event)"></el-input>
+      <!-- 密码 -->
+      <div class="el-input-div" style="margin-top: 20px;">
+        <el-input v-model="loginInput.pwdValue" placeholder="请输入密码" :prefix-icon="Lock" @input="pwdChangeHandle($event)"  show-password></el-input>
       </div>
 
-<!--   验证   -->
-      <div class="div-el-input" style="margin-top: 20px;">
-        <!-- 验证码输入框-->
-        <el-input v-model="codeInputValue" placeholder="请输入验证码" :prefix-icon="Check" style="width: calc(100%  - 110px);vertical-align:middle;float: left;" @input="codeChangeHandle($event)"></el-input>
-        <!--图形验证码-->
-        <div class="login_code" @click="refreshCode">
-          <imageValidate :contentWidth=100 :contentHeight=40 style="float: right"  :identifyCode="identifyCode"></imageValidate>
+      <!-- 图片验证码 -->
+      <div class="el-input-div" style="margin-top: 20px;">
+        <!-- 输入框 -->
+        <div class="imgCode-input">
+          <el-input  v-model="loginInput.codeInputValue" placeholder="请输入验证码" :prefix-icon="Check" @input="codeChangeHandle($event)"></el-input>
         </div>
 
+        <!-- 图形 -->
+        <div class="imgCode-imageValidate-div" @click="refreshCode">
+          <imageValidate :contentWidth=100 :contentHeight=40 :identifyCode="identifyCode"></imageValidate>
+        </div>
       </div>
-<!--登录按钮-->
-      <div style="width: 100%;height: auto;display: flex;justify-content: center;">
-      <el-button id="loginModuleDIV-loginButton" type="primary" @click="loginActionFunc">登 录</el-button>
+
+      <!--登录按钮-->
+      <div class="login-button-div">
+      <el-button class="login-button" type="primary" @click="loginActionFunc">登 录</el-button>
       </div>
-<!--      注册按钮-->
-      <div style="width: auto;height: auto">
-        <button id="loginModuleDIV-registerButton" @click="registerActionFunc">注 册</button>
+
+      <!-- 注册按钮-->
+      <div>
+        <button class="lmd-register-button" @click="registerActionFunc">注 册</button>
       </div>
 
     </div>
@@ -45,96 +48,95 @@
 </template>
 
 
-<script>
-export default {
-  data() {
-    return {
-      //这里一定要记得返回v-model中绑定的值！
-      nameValue: '',
-      pwdValue:'',
-      codeInputValue:'',
-    }
-  }
-}
-</script>
+<!--<script>-->
+<!--export default {-->
+<!--  data() {-->
+<!--    return {-->
+<!--      //这里一定要记得返回v-model中绑定的值！-->
+<!--      nameValue: '',-->
+<!--      pwdValue:'',-->
+<!--      codeInputValue:'',-->
+<!--    }-->
+<!--  }-->
+<!--}-->
+<!--</script>-->
 
 <script setup>
 import { Avatar, Lock, Check } from "@element-plus/icons-vue";
+
 import imageValidate from "@/components/ssj-image-validate.vue";
 
-// loginInput:{
-//   let nameValue = "",
-//       pwdValue = ""
-// }
+// 定义一个对象，用来存放输入的账号、密码、验证码
+let loginInput = {
+      nameValue: '',
+      pwdValue: '',
+      codeInputValue: ''
+}
 
-let nameValue = ""
-let pwdValue = ""
-let codeInputValue = ""
 
 // 图形验证码
-let identifyCodes = "1234567890"
-let identifyCode = ref('3212')
+let identifyCodes = "1234567890abcdefghijklmnopqrstuvwxyz"//随机数-数据源
+let identifyCode = ref('3212')//当前随机数，初始值为3212
 
 import router from "../router";
 // import SSJButton from "@/components/SSJButton";
 import {getRegisterCode, loginWithUNameAndPwd} from "@/api/api";
 import {getCurrentInstance, onMounted, ref} from 'vue'
+import Home from "@/views/Home";
 
 // let codeInputRef = ref(null)
 
 
 // const datab = getCurrentInstance();
 
-let uName = ""
 
 //登录按钮 - 点击
-function loginActionFunc(){
-      const  e = ref(uName)
-  console.log("点击了按钮:"+e.value)
-  //     let  uName = ref(null)
-  console.log("账号:"+nameValue)
-  console.log("密码:"+pwdValue.valueOf())
+function loginActionFunc() {
+  // const  e = ref(uName)
+  // console.log("点击了按钮:"+e.value)
+  // //     let  uName = ref(null)
+  // console.log("账号:"+nameValue)
+  // console.log("密码:"+pwdValue.valueOf())
 
+  console.log("输入的验证码:" + loginInput.codeInputValue)
 
-  console.log("输入的验证码:"+codeInputValue)
+  if (loginInput.codeInputValue == identifyCode.value) {
+    //开始请求
+    loginWithUNameAndPwd({"username": loginInput.nameValue, "password": loginInput.pwdValue}).then(
+        (res) => {
+          console.log("请求结束了\\n")
+          console.log(res)
+          alert("登录成功，即将跳转首页")
+          router.push("/home")
+        }
+    ).catch((err) => {
+      // console.log("请求错误信息："+ err.message)
+      if (err.message.includes("code 500")) {
+        alert("500错误，请联系管理员")
+      } else {
+        alert("其它错误：" + err.message)
+      }
+    })
 
-  if (codeInputValue == identifyCode.value) {
-    alert("验证码-校验成功")
-  }else {
-    alert("您输入的验证码不对："+codeInputValue+"\t 应该是:"+identifyCode.value)
+  } else {
+    alert("您输入的验证码不对：" + loginInput.codeInputValue + "\t 应该是:" + identifyCode.value)
+    return
   }
-
-
-
-      // this.$emit()
-      // alert("登录成功！即将跳转首页")
-      // router.push("/home")
-
-      // getRegisterCode({"username":this.$refs.uName.textContent}).then(
-      //     (res) => {
-      //       console.log("请求结束了\\n")
-      //       console.log(res)
-      //     }
-      // ).catch((err) =>{
-      //   console.log("请求错误信息："+ err)
-      // })
-
-      // //开始请求
-      // loginWithUNameAndPwd({"username":this.$refs.uName.textContent, "password": this.$refs.pwd.textContent}).then(
-      //     (res) => {
-      //       console.log("请求结束了\\n")
-      //       console.log(res)
-      //     }
-      // ).catch((err) =>{
-      //   console.log("请求错误信息："+ err)
-      // })
-
-    }
+}
 
 //注册按钮-点击
 function registerActionFunc(){
 // console.log("注册模块，还没开始写")
   alert("注册模块，还没开始写")
+
+  // getRegisterCode({"username":this.$refs.uName.textContent}).then(
+  //     (res) => {
+  //       console.log("请求结束了\\n")
+  //       console.log(res)
+  //     }
+  // ).catch((err) =>{
+  //   console.log("请求错误信息："+ err)
+  // })
 }
 
 //定义在方法外面，才有效
@@ -143,30 +145,25 @@ const currentInstance = getCurrentInstance()
 //账号-内容变化
 function changeHandle (e) { // input事件
   // console.log("changeHandle1:"+ document.getElementById('inputUname').value)
-  console.log("changeHandle->nameValue:"+ nameValue +"\t e:"+e)
+  console.log("changeHandle->nameValue:"+ loginInput.nameValue +"\t e:"+e)
   currentInstance.proxy.$forceUpdate()
-  nameValue = e
+  loginInput.nameValue = e
 }
 
 //密码-内容变化
 function pwdChangeHandle (e) { // input事件
                             // console.log("changeHandle1:"+ document.getElementById('inputUname').value)
-  console.log("pwdChangeHandle->pwdValue:"+ pwdValue +"\t e:"+e)
+  console.log("pwdChangeHandle->pwdValue:"+ loginInput.pwdValue +"\t e:"+e)
   currentInstance.proxy.$forceUpdate()
-pwdValue = e
+  loginInput.pwdValue = e
 }
 
 //验证码-内容变化
 // let codeInput = ref(null)
 function codeChangeHandle(e){
-
-
-  // console.log("输入的验证码:"+codeInput.value)
-
-  // console.log("codeChangeHandle->pwdValue:"+ codeInputValue +"\t e:"+ e +"\t 通过ref获取验证码："+codeInputRef.value)
-  console.log("codeChangeHandle->pwdValue:"+ codeInputValue +"\t e:"+ e)
-currentInstance.proxy.$forceUpdate()
-  codeInputValue = e
+  console.log("codeChangeHandle->pwdValue:"+ loginInput.codeInputValue +"\t e:"+ e)
+  currentInstance.proxy.$forceUpdate()
+  loginInput.codeInputValue = e
 }
 
 
@@ -194,7 +191,7 @@ const makeCode = (o, l) => {
 // };
 
 onMounted( ()=> {
-  console.log("onMounted---codeInputRef---",codeInputValue.value)
+  console.log("onMounted---codeInputRef---",loginInput.codeInputValue.value)
 });
 
 </script>
@@ -203,7 +200,7 @@ onMounted( ()=> {
 <style>
 
 /* 背景div */
-#backgroundDIV{
+#background-div{
   background-color: bisque;
   width: 100%;
   height: 100%;
@@ -211,18 +208,19 @@ onMounted( ()=> {
 }
 
 /* 背景图片 */
-#backgroundDIV img{
+#background-div img{
   width: 100%;height: 100%;
 }
 
 /* 登录模块 */
-#loginModuleDIV{
+#login-module-div{
   /*background-color:#323bb6;*/
 
   /*! autoprefixer: ignore next */
   background:-webkit-gradient(linear, 100% 0, 0 0, from(rgb(58 96 199)), to(rgb(57 63 187)));
 
-  width: 40%;height: 65%;
+  width: 35%;
+  height: 540px;
   position: absolute;
   top: 50%;
   left:50%;
@@ -232,29 +230,38 @@ onMounted( ()=> {
 }
 
 /* 主标题 */
-.loginModuleDIV-title{
+#login-module-div .lmd-title{
   color: rgba(255,255,255,0.9);
   padding: 0;
   margin: 40px 0 0 0;
 }
 
 /* 副标题 */
-.loginModuleDIV-subtitle{
+#login-module-div .lmd-subtitle{
   color: rgba(255,255,255,0.9);
   padding: 0;
   margin: 30px 0 0 0;
 }
 
 /*账号、密码div公共样式设置：*/
-.div-el-input{
+.el-input-div{
   width: 100%;padding: 0px 20px 0px 20px;box-sizing: border-box;
 }
 
 /*el-input内部实际上也用到了input组件，所以这里的设置，对账号和密码这两个el-input也有效果*/
-#loginModuleDIV input{
+#login-module-div input{
   height: 40px;
   border-radius: 10px;/*设置四分之一圆角*/
   border: 0px solid rgb(255,255,255);/* border-radius要配合这句使用，否则输入框会出现内阴影*/
+}
+
+/*图片验证码 - 输入框样式*/
+#login-module-div .imgCode-input{
+  width: calc(100%  - 110px);vertical-align:middle;float: left;
+}
+/*图片验证码 - 图片样式*/
+.imgCode-imageValidate-div{
+  float: right;
 }
 
 /*>>> .el-input__inner{*/
@@ -268,7 +275,11 @@ onMounted( ()=> {
 /*}*/
 
 /* 登录按钮 */
-#loginModuleDIV-loginButton{
+#login-module-div .login-button-div{
+  width: 100%;height: auto;display: flex;justify-content: center;
+}
+
+#login-module-div .login-button-div .login-button{
  margin-top: 48px;
   width: 45%;
   height: 47px;
@@ -276,7 +287,7 @@ onMounted( ()=> {
 }
 
 /* 注册按钮 */
-#loginModuleDIV-registerButton{
+#login-module-div .lmd-register-button{
   margin-top: 27px;
   /*按钮设置透明：background-color + border-width */
   background-color: rgba(0,0,0,0);
